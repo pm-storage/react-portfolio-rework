0647fd61ce416839e16fa030c78863e32a107ad2
Plamen Mitev <o0pmitev@linux.fritz.box>
2019-09-17 10:16:47 +0200

    Updates

---
+++ precache-manifest.baf4a9059c7440023cab90661f05642d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc8e3a1fdd9545d05984cbcaf651a10",
+    "url": "/react-portfolio-rework/index.html"
+  },
+  {
+    "revision": "1dc19780fa24648b9dcc",
+    "url": "/react-portfolio-rework/static/css/main.d40ebc2e.chunk.css"
+  },
+  {
+    "revision": "5451167cdf92815e245c",
+    "url": "/react-portfolio-rework/static/js/2.d98bcbff.chunk.js"
+  },
+  {
+    "revision": "1dc19780fa24648b9dcc",
+    "url": "/react-portfolio-rework/static/js/main.c8de5a0a.chunk.js"
+  },
+  {
+    "revision": "61726237b67181196923",
+    "url": "/react-portfolio-rework/static/js/runtime~main.8bbf021d.js"
+  }
+]);
